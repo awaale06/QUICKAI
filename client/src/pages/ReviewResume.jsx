c53d3e6a1486b226c733088e1827efe2d9aea742
awaale06 <yasirabdi91@gmail.com>
2025-07-15 19:45:13 +0300

    Updated Community page

--- client/src/pages/ReviewResume.jsx
+++ client/src/pages/ReviewResume.jsx
@@ -23,7 +23,7 @@ const ReviewResume = () => {
           <button className='w-full flex justify-center items-center gap-2 bg-gradient-to-r from-[#00DA83] to-[#009BB3] text-white px-4 py-2 mt-6 text-sm rounded-lg cursor-pointer'> <FileText className='w-5' /> Review Resume</button>
       </form>
 
-      <div className='w-full max-w-lg p-4 bg-white rounded-lg flex flex-col border border-gray-200 min-h-96 '>
+      <div className='w-full max-w-lg p-4 bg-white rounded-lg flex flex-col border border-gray-200 min-h-96  max-h-[600px]'>
         <div className='flex items-center gap-3'>
           <FileText className='w-5 h-5 text-[#00DA83]' />
           <h1 className='text-xl font-semibold'>Analysis Results</h1>
